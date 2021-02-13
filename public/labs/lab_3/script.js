5a63f4c797527addb9d5087421ab3a2d7140ae2d
hchau98 <chauhenry02@gmail.com>
2021-02-12 20:49:50 -0500

    EDIT

---
+++ public/labs/lab_3/script.js
@@ -0,0 +1,31 @@
+/* label the images, just for convenience, to visually track them */
+const images = document.querySelectorAll('.imageClass');
+const imageArray = Array.from(images);
+imageArray.forEach((element) => {
+    console.log(element)
+    })
+
+/* configuration */
+let width = 130; // image width
+let count = 3; // visible images count
+
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
+
+let position = 0; // ribbon scroll position
+
+carousel.querySelector('.prev').onclick = function() {
+  // shift left
+  position += width * count;
+  // can't move to the left too much, end of images
+  position = Math.min(position, 0)
+  list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.next').onclick = function() {
+  // shift right
+  position -= width * count;
+  // can only shift the ribbbon for (total ribbon length - visible count) images
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + 'px';
+};
